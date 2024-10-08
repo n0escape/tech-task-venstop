d3736b9c9c4e42a42e4c6a78b2f936049783681a
Dmitriy Zavadovskyi <dmitrijzavadoskij@gmail.com>
2024-10-08 15:47:41 +0300

    fixed img src 2

--- assets/scripts/hoverDarkLink.js
+++ assets/scripts/hoverDarkLink.js
@@ -1,13 +1,13 @@
 const originalSrcArray = [
-    '/assets/images/icons/follow-sloclap-icon.png',
-    '/assets/images/icons/follow-kepler-icon.png',
-    '/assets/images/icons/follow-unreal-icon.png'
+    'assets/images/icons/follow-sloclap-icon.png',
+    'assets/images/icons/follow-kepler-icon.png',
+    'assets/images/icons/follow-unreal-icon.png'
 ];
 
 const hoverSrcArray = [
-	'/assets/images/icons/follow-sloclap-icon-hover.png',
-	'/assets/images/icons/follow-kepler-icon-hover.png',
-	'/assets/images/icons/follow-unreal-icon-hover.png'
+	'assets/images/icons/follow-sloclap-icon-hover.png',
+	'assets/images/icons/follow-kepler-icon-hover.png',
+	'assets/images/icons/follow-unreal-icon-hover.png'
 ];
 
 export const inputDarkLogos = () => {
